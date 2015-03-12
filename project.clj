d76218437831228cb0b536e6b702df940c6921d2
Yasuhisa Yoshida <syou6162@gmail.com>
2015-03-12 13:31:28 +0900

    メモリを確保する量を増やした

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.1"]
-                 [edu.stanford.nlp/stanford-parser "2.0.5"]])
+                 [edu.stanford.nlp/stanford-parser "2.0.5"]]
+  :jvm-opts ["-Xmx3g" "-server" "-Dfile.encoding=UTF-8"])
